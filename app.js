3aacae809c09fdcba82af17f7490948698c8a67e
ericdo <ericdo.617@gmail.com>
2019-04-22 12:35:04 -0700

    Solve solo exercise

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-// TODO
+var GroceryList = (props) => (
+  <ul>
+    {
+      props.items.map(item => (
+        <GroceryListItem key={item.toString()} name={item} />
+      ))
+    }
+  </ul>
+);
+
+// var GroceryListItem = (props) => (
+//   <li>{props.name}</li>
+// );
+
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hover: false
+    }
+  }
+
+  onListItemEnter() {
+    console.log('Mouse has entered');
+    this.setState({
+      hover: true
+    });
+  }
+
+  onListItemLeave() {
+    console.log('Mouse has left');
+    this.setState({
+      hover: false
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.hover === true ? 'underline' : 'none'
+    }
+    return (
+      <li style={style} 
+          onMouseEnter={this.onListItemEnter.bind(this)} 
+          onMouseLeave={this.onListItemLeave.bind(this)}>
+          {this.props.name}
+      </li>
+    );
+  }
+}
+
+ReactDOM.render(<GroceryList items={['Bananas', 'Apples']} />, document.getElementById("app"));
+
